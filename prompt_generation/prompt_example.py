967db0e103e2a363aae81867c683d8fce0b48f8c
hua <113157614+huy222-lu@users.noreply.github.com>
2025-10-08 02:02:37 -0400

    Update prompt_example.py

--- prompt_generation/prompt_example.py
+++ prompt_generation/prompt_example.py
@@ -1,1 +1,9 @@
 
+if __name__ == "__main__":
+    
+    data = np.load("uci/data_20_120.npy")
+    acc  = data[0, :, :3].astype(float)   
+    gyro = data[0, :, 3:6].astype(float)   
+    dataset_name = "uci"
+    prompt = generate_promt(acc, gyro, dataset_name, '')
+    print(prompt)
